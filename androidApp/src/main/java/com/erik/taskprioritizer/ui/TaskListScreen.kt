3e4350b4ca73b2233c96bf687a97914ffde8fcdd
Erik Macák <erikmacak026@gmail.com>
2025-05-20 19:27:15 +0200

    create task list screen

---
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/TaskListScreen.kt
@@ -0,0 +1,9 @@
+package com.erik.taskprioritizer.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun TaskListScreen() {
+    Text(text = "Tasks should show here")
+}
