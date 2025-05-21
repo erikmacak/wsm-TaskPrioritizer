d3a14011075a02e63eaf0fdc18bf157551840e10
Erik Macák <erikmacak026@gmail.com>
2025-05-21 12:56:29 +0200

    BackgroundColor renamed to ContainerBackgroundColor

--- androidApp/src/main/java/com/erik/taskprioritizer/ui/theme/Color.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.erik.taskprioritizer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BackgroundColor = Color(0xFF121212)
+val ContainerBackgroundColor = Color(0xFF121212)
 val Blue = Color(0xFF007AFF)
 val White = Color(0xFFFFFFFF)
 val BackgroundGray = Color(0xFF333333)
