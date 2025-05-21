3cd244d8f2ae0e58ad5face3a48bbebfa502fc1e
Erik Macák <erikmacak026@gmail.com>
2025-05-21 23:54:59 +0200

    PriorityTasksListScreen file initialization

---
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/PriorityTasksListScreen.kt
@@ -0,0 +1,53 @@
+package com.erik.taskprioritizer.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.erik.taskprioritizer.ui.theme.Montserrat
+
+@Composable
+fun PriorityTasksListScreen() {
+    var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
+
+    val tasks = listOf(
+        "Fix landing page",
+        "Share prototype with team",
+        "Add unit tests",
+        "Add e2e tests",
+        "Configure db connection"
+    )
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(16.dp)
+        ) {
+
+        Text(
+            text = "Sorted By Priorities",
+            fontSize = 24.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Black,
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(bottom = 16.dp)
+        )
+    }
+
+}
