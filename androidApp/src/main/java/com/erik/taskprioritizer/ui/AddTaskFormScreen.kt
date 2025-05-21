4d969787998e35e31ea5a9fc9036bdfcdfcf1695
Erik Macák <erikmacak026@gmail.com>
2025-05-21 18:02:22 +0200

    Finished add task form screen UI

--- androidApp/src/main/java/com/erik/taskprioritizer/ui/AddTaskFormScreen.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/AddTaskFormScreen.kt
@@ -3,20 +3,198 @@ package com.erik.taskprioritizer.ui
 import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.erik.taskprioritizer.ui.theme.Montserrat
+import com.erik.taskprioritizer.ui.theme.ContainerBackgroundColor
+import com.erik.taskprioritizer.ui.theme.BackgroundGray
+import com.erik.taskprioritizer.ui.theme.Blue
+import com.erik.taskprioritizer.ui.theme.Green
+import com.erik.taskprioritizer.ui.theme.Montserrat
+import com.erik.taskprioritizer.ui.theme.Orange
+import com.erik.taskprioritizer.ui.theme.TextGray
+import com.erik.taskprioritizer.ui.components.CustomSlider
 
+@ExperimentalMaterial3Api
 @Composable
 fun AddTaskFormScreen () {
-    Row(
+    var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column (
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.White)
+            .background(Color.Black)
+            .padding(16.dp)
     ) {
+
         Text(
-            "Add Task Form Screen should appear",
-            color = Color.Black
+            text = "Add Task",
+            fontSize = 24.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Black,
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(bottom = 16.dp)
         )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = "Task Name:",
+            fontSize = 20.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        TextField(
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+            placeholder = {
+                Text(
+                    text = "Create PR",
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Bold,
+                    color = TextGray
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = BackgroundGray,
+                unfocusedContainerColor = BackgroundGray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedTextColor = Color.White,
+                unfocusedTextColor = Color.White
+            ),
+            singleLine = true,
+            shape = RoundedCornerShape(20.dp)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Benefit: ",
+            fontSize = 20.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+
+        var benefit by remember { mutableFloatStateOf(0f) }
+
+        CustomSlider(
+            value = benefit,
+            onValueChange = { benefit = it }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Complexity: ",
+            fontSize = 20.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+
+        var complexity by remember { mutableFloatStateOf(0f) }
+
+        CustomSlider(
+            value = complexity,
+            onValueChange = { complexity = it}
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Urgency: ",
+            fontSize = 20.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+
+        var urgency by remember { mutableFloatStateOf(0f) }
+
+        CustomSlider(
+            value = urgency,
+            onValueChange = { urgency = it}
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Benefit: ",
+            fontSize = 20.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+
+        var risk by remember { mutableFloatStateOf(0f) }
+
+        CustomSlider(
+            value = risk,
+            onValueChange = { risk = it}
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TextButton(onClick = { /* TODO: zpět akce */ }) {
+                Text(
+                    text = "BACK",
+                    color = Blue,
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            TextButton(onClick = { /* TODO: uložení akce */ }) {
+                Text(
+                    text = "SAVE",
+                    color = Green,
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+        }
     }
 }
