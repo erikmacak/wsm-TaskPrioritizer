29bb8996e2241c3384d30b58ccbe4dd237e24ce4
Erik Macák <erikmacak026@gmail.com>
2025-05-21 12:45:26 +0200

    Custom font and colors added, custom theme created

---
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.erik.taskprioritizer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundColor = Color(0xFF121212)
+val Blue = Color(0xFF007AFF)
+val White = Color(0xFFFFFFFF)
+val BackgroundGray = Color(0xFF333333)
+val TextGray = Color(0xFF797979)
+val Green = Color(0xFF05FF6F)
+val Orange = Color(0xFFFF6F00)
+val Red = Color(0xFFFF0000)
---
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/theme/Theme.kt
@@ -0,0 +1,11 @@
+package com.erik.taskprioritizer.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskPrioritizerTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        content = content
+    )
+}
