bc412bf9a5d585e09d433231b2474d535ca9144d
Erik Macák <erikmacak026@gmail.com>
2025-05-22 14:16:35 +0200

    ExportPossibilities component finished

--- androidApp/src/main/java/com/erik/taskprioritizer/ui/components/ExportPossibilities.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/components/ExportPossibilities.kt
@@ -1,8 +1,32 @@
 package com.erik.taskprioritizer.ui.components
 
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.erik.taskprioritizer.ui.theme.Blue
+import com.erik.taskprioritizer.ui.theme.Montserrat
 
 @Composable
 fun ExportPossibilities () {
+    TextButton(onClick = { /* TODO: uložení akce */ }) {
+        Text(
+            text = "CSV",
+            color = Blue,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+    }
 
+    TextButton(onClick = { /* TODO: uložení akce */ }) {
+        Text(
+            text = "JSON",
+            color = Blue,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+    }
 }
