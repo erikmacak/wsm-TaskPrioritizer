30c333888df110e8920afbe893d481c1ffdf04bc
Erik Macák <erikmacak026@gmail.com>
2025-05-22 14:18:39 +0200

    Unused imports deleted

--- androidApp/src/main/java/com/erik/taskprioritizer/ui/PriorityTasksListScreen.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/PriorityTasksListScreen.kt
@@ -1,6 +1,5 @@
 package com.erik.taskprioritizer.ui
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,18 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,7 +22,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.erik.taskprioritizer.ui.components.IntroductoryText
@@ -42,12 +29,7 @@ import com.erik.taskprioritizer.ui.components.SearchBar
 import com.erik.taskprioritizer.ui.components.SelectableTabButton
 import com.erik.taskprioritizer.ui.components.RankedTaskItemCard
 import com.erik.taskprioritizer.ui.components.ExportPossibilities
-import com.erik.taskprioritizer.ui.theme.BackgroundGray
-import com.erik.taskprioritizer.ui.theme.Blue
-import com.erik.taskprioritizer.ui.theme.ContainerBackgroundColor
-import com.erik.taskprioritizer.ui.theme.Green
 import com.erik.taskprioritizer.ui.theme.Montserrat
-import com.erik.taskprioritizer.ui.theme.TextGray
 
 @Composable
 fun PriorityTasksListScreen() {
