833f8c75e0e9c7fad91e743291e75605b57fbe12
Erik Macák <erikmacak026@gmail.com>
2025-05-22 10:30:56 +0200

    PriorityTasksListScreen UI finished

--- androidApp/src/main/java/com/erik/taskprioritizer/ui/PriorityTasksListScreen.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/PriorityTasksListScreen.kt
@@ -1,10 +1,30 @@
 package com.erik.taskprioritizer.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +37,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.erik.taskprioritizer.ui.theme.BackgroundGray
+import com.erik.taskprioritizer.ui.theme.Blue
+import com.erik.taskprioritizer.ui.theme.ContainerBackgroundColor
+import com.erik.taskprioritizer.ui.theme.Green
 import com.erik.taskprioritizer.ui.theme.Montserrat
+import com.erik.taskprioritizer.ui.theme.TextGray
 
 @Composable
 fun PriorityTasksListScreen() {
@@ -48,6 +73,152 @@ fun PriorityTasksListScreen() {
                 .align(Alignment.CenterHorizontally)
                 .padding(bottom = 16.dp)
         )
-    }
 
+        Spacer(modifier = Modifier.height(12.dp))
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+        ) {
+            TextField(
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+                placeholder = {
+                    Text(
+                        "Search",
+                        fontFamily = Montserrat,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = BackgroundGray,
+                    unfocusedContainerColor = BackgroundGray,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White
+                ),
+                singleLine = true,
+                shape = RoundedCornerShape(20.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 48.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                "All",
+                color = Color.White,
+                fontFamily = Montserrat,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.CenterVertically))
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(containerColor = Blue),
+                shape = RoundedCornerShape(20.dp)
+            ) {
+                Text(
+                    "Priorities",
+                    color = Color.White,
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterVertically))
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+
+        LazyColumn(
+            modifier = Modifier.weight(1f)
+        ) {
+            items(tasks) { task ->
+                TaskItemWithNumbers(taskTitle = task)
+                Spacer(modifier = Modifier.height(24.dp))
+            }
+        }
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = "Export to:",
+                color = Color.White,
+                fontFamily = Montserrat,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+
+            TextButton(onClick = { /* TODO: uložení akce */ }) {
+                Text(
+                    text = "CSV",
+                    color = Blue,
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            TextButton(onClick = { /* TODO: uložení akce */ }) {
+                Text(
+                    text = "JSON",
+                    color = Blue,
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Override
+fun TaskItemWithNumbers(taskTitle: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(horizontal = 16.dp),
+        shape = RoundedCornerShape(12.dp),
+        border = BorderStroke(1.dp, Green),
+        colors = CardDefaults.cardColors(
+            containerColor = ContainerBackgroundColor
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = taskTitle,
+                color = Color.White,
+                fontFamily = Montserrat,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "#1",
+                color = TextGray,
+                fontFamily = Montserrat,
+                fontWeight = FontWeight.Black
+            )
+        }
+    }
 }
