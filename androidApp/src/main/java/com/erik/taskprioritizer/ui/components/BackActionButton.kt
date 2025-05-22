d62efd4e44a717540d4c5d1e803c68e4c4568262
Erik Macák <erikmacak026@gmail.com>
2025-05-22 14:58:59 +0200

    BackActionButton component added and finished

---
+++ androidApp/src/main/java/com/erik/taskprioritizer/ui/components/BackActionButton.kt
@@ -0,0 +1,23 @@
+package com.erik.taskprioritizer.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.erik.taskprioritizer.ui.theme.Blue
+import com.erik.taskprioritizer.ui.theme.Green
+import com.erik.taskprioritizer.ui.theme.Montserrat
+
+@Composable
+fun BackActionButtons() {
+    TextButton(onClick = { /* zpět akce */ }) {
+        Text(
+            text = "BACK",
+            color = Blue,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+    }
+}
