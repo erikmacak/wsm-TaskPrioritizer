c7e58ef9e6129c280b53c48fcbd0586724291902
Erik Macák <erikmacak026@gmail.com>
2025-05-24 13:36:28 +0200

    Private attributes added to every Task property

--- shared/src/commonMain/kotlin/com/erik/taskprioritizer/logic/CalculateTaskScoring.kt
+++ shared/src/commonMain/kotlin/com/erik/taskprioritizer/logic/CalculateTaskScoring.kt
@@ -4,6 +4,6 @@ import com.erik.taskprioritizer.model.Task
 import com.erik.taskprioritizer.model.Weights
 
 fun calculateTaskScoring(task: Task, weights: Weights): Float {
-    return (task.benefit * weights.benefit) - (task.complexity * weights.complexity) +
-            (task.urgency * weights.urgency) - (task.risk * weights.risk)
+    return (task.getBenefit() * weights.getBenefit()) - (task.getComplexity() * weights.getComplexity()) +
+            (task.getUrgency() * weights.getUrgency()) - (task.getRisk() * weights.getRisk())
 }
--- shared/src/commonMain/kotlin/com/erik/taskprioritizer/model/Task.kt
+++ shared/src/commonMain/kotlin/com/erik/taskprioritizer/model/Task.kt
@@ -3,13 +3,13 @@ package com.erik.taskprioritizer.model
 import com.erik.taskprioritizer.util.generateUUID
 
 data class Task (
-    val id: String = generateUUID(),
-    val name: String,
-    val benefit: Int,
-    val complexity: Int,
-    val urgency: Int,
-    val risk: Int,
-    var isExpanded: Boolean = false
+    private val id: String = generateUUID(),
+    private val name: String,
+    private val benefit: Int,
+    private val complexity: Int,
+    private val urgency: Int,
+    private val risk: Int,
+    private var isExpanded: Boolean = false
 ) {
     fun getTitle(): String {
         return name
