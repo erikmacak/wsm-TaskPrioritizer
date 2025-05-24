4377a2ddaa634bc31a94c426703ba9105d4ece68
Erik Macák <erikmacak026@gmail.com>
2025-05-24 16:48:58 +0200

    Dependency for navigation added

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation(projects.shared)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
