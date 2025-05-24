109c5e274a6698986b390f751755b0de56781079
Erik Macák <erikmacak026@gmail.com>
2025-05-24 17:07:04 +0200

    Navigation and lifecycle compose libraries changed to 2.8.0 versions

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -39,8 +39,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation(projects.shared)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
