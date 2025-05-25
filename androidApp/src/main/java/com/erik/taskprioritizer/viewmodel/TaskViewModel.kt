1330a6151b2638662cc13f5c076981bd88fde49f
Erik Macák <erikmacak026@gmail.com>
2025-05-25 14:47:55 +0200

    Added functions for managing tasks within the application

--- androidApp/src/main/java/com/erik/taskprioritizer/viewmodel/TaskViewModel.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/viewmodel/TaskViewModel.kt
@@ -1,10 +1,27 @@
 package com.erik.taskprioritizer.viewmodel
 
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.toMutableStateMap
 import androidx.lifecycle.ViewModel
+import com.erik.taskprioritizer.model.Task
+import com.erik.taskprioritizer.model.Weights
+import com.erik.taskprioritizer.repository.TaskRepository
+import com.erik.taskprioritizer.repository.WeightsRepository
 
 class TaskViewModel: ViewModel() {
+    private val taskRepo = TaskRepository()
+
+    fun addTask(task: Task) = taskRepo.add(task)
+
+    fun updateTask(task: Task) = taskRepo.update(task)
+
+    fun getTasks(): List<Task> = taskRepo.getAll()
+
+    fun getTaskById(id: String): Task? = taskRepo.findById(id)
+
+    fun removeTask(id: String) = taskRepo.removeById(id)
+
+    fun clearTasks() = taskRepo.clear()
+
     val expandedStates = mutableStateMapOf<String, Boolean>()
 
     fun toggleExpanded(taskId: String) {
