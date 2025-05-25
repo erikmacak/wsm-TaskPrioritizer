cad83f73271ba94a7a779d16396146a187eb72a7
Erik Macák <erikmacak026@gmail.com>
2025-05-25 12:54:40 +0200

    Enabled functions for weights management in WeightsRepository.kt file

--- shared/src/commonMain/kotlin/com/erik/taskprioritizer/repository/WeightsRepository.kt
+++ shared/src/commonMain/kotlin/com/erik/taskprioritizer/repository/WeightsRepository.kt
@@ -3,17 +3,11 @@ package com.erik.taskprioritizer.repository
 import com.erik.taskprioritizer.model.Weights
 
 class WeightsRepository {
-    private val weightsList = mutableListOf<Weights>()
+    private var weightsList: Weights = Weights(urgency = 0.25f, risk = 0.2f, complexity = 0.3f, benefit = 0.25f)
 
-    fun add(weights: Weights) {
-        weightsList.add(weights)
-    }
+    fun get(): Weights = weightsList
 
-    fun getAll(): List<Weights> {
-        return weightsList.toList()
-    }
-
-    fun clear() {
-        weightsList.clear()
+    fun update(newWeightsValue: Weights) {
+        weightsList= newWeightsValue
     }
 }
