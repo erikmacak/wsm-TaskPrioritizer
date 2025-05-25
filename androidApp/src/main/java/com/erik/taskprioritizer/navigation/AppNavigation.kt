464033ef8817d00faa53f4761b0055a17b64570a
Erik Macák <erikmacak026@gmail.com>
2025-05-25 14:53:45 +0200

    Passing ViewModels to TaskListScreen and implemented task addition to repository

--- androidApp/src/main/java/com/erik/taskprioritizer/navigation/AppNavigation.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/navigation/AppNavigation.kt
@@ -1,7 +1,9 @@
 package com.erik.taskprioritizer.navigation
 
+import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
@@ -11,10 +13,17 @@ import com.erik.taskprioritizer.ui.TaskListScreen
 import com.erik.taskprioritizer.ui.EditTaskFormScreen
 import com.erik.taskprioritizer.ui.PriorityTasksListScreen
 
+import com.erik.taskprioritizer.model.Task
+import com.erik.taskprioritizer.repository.TaskRepository
+import com.erik.taskprioritizer.viewmodel.TaskViewModel
+import com.erik.taskprioritizer.viewmodel.WeightsViewModel
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
+    val taskViewModel: TaskViewModel = viewModel()
+    val weightsViewModel: WeightsViewModel = viewModel()
 
     NavHost(
         navController = navController,
@@ -22,6 +31,8 @@ fun AppNavigation() {
     ) {
         composable(NavigationDestination.TaskList.route) {
             TaskListScreen (
+                taskViewModel = taskViewModel,
+                weightsViewModel = weightsViewModel,
                 onEditClick = {
                     navController.navigate(NavigationDestination.EditTask.route)
                 },
@@ -57,6 +68,17 @@ fun AppNavigation() {
             AddTaskFormScreen(
                 onBackClick = {
                     navController.navigate(NavigationDestination.TaskList.route)
+                },
+                onSaveClick = { taskName, criteriaValues ->
+                    val task = Task(
+                        name = taskName,
+                        benefit = (criteriaValues["Benefit"] ?: 0f).toInt(),
+                        complexity = (criteriaValues["Complexity"] ?: 0f).toInt(),
+                        urgency = (criteriaValues["Urgency"] ?: 0f).toInt(),
+                        risk = (criteriaValues["Risk"] ?: 0f).toInt()
+                    )
+                    taskViewModel.addTask(task)
+                    navController.popBackStack()
                 }
             )
         }
