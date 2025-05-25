7334608404a8eb276083006106f70d1e35bb01b2
Erik Macák <erikmacak026@gmail.com>
2025-05-25 17:07:35 +0200

    Navigation destination for remove tasks added

--- androidApp/src/main/java/com/erik/taskprioritizer/navigation/NavigationDestination.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/navigation/NavigationDestination.kt
@@ -5,5 +5,6 @@ sealed class NavigationDestination(val route: String) {
     object PriorityTaskList: NavigationDestination("priority_task_list")
     object AddTask: NavigationDestination("add_task")
     object EditTask : NavigationDestination("edit_task")
+    object RemoveTask: NavigationDestination("remove_task")
     object AdjustWeights: NavigationDestination("adjust_weights")
 }
