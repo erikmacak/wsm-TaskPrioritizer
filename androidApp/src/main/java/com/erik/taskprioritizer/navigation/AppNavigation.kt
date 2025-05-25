7ccefa02a779ce1ed0eb24da5baa92d07627eb0d
Erik Macák <erikmacak026@gmail.com>
2025-05-25 16:55:32 +0200

    Implemented proper edit navigation handling

--- androidApp/src/main/java/com/erik/taskprioritizer/navigation/AppNavigation.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/navigation/AppNavigation.kt
@@ -33,8 +33,8 @@ fun AppNavigation() {
             TaskListScreen (
                 taskViewModel = taskViewModel,
                 weightsViewModel = weightsViewModel,
-                onEditClick = {
-                    navController.navigate(NavigationDestination.EditTask.route)
+                onEditClick = { taskId ->
+                    navController.navigate("${NavigationDestination.EditTask.route}/$taskId")
                 },
                 onPrioritiesClick = {
                     navController.navigate(NavigationDestination.PriorityTaskList.route)
@@ -48,12 +48,31 @@ fun AppNavigation() {
             )
         }
 
-        composable(NavigationDestination.EditTask.route) {
-            EditTaskFormScreen(
-                onBackClick = {
-                    navController.navigate(NavigationDestination.TaskList.route)
-                }
-            )
+        composable("${NavigationDestination.EditTask.route}/{taskId}") { backStackEntry ->
+            val taskId = backStackEntry.arguments?.getString("taskId")
+
+            taskId?.let { id ->
+                val task = taskViewModel.getTaskById(id)
+                EditTaskFormScreen(
+                    task = task,
+                    onBackClick = {
+                        navController.navigate(NavigationDestination.TaskList.route)
+                    },
+                    onSaveClick = { taskName, criteriaValues ->
+                        if (task != null) {
+                            val updatedTask = task.copy(
+                                name = taskName,
+                                benefit = (criteriaValues["Benefit"] ?: 0f).toInt(),
+                                complexity = (criteriaValues["Complexity"] ?: 0f).toInt(),
+                                urgency = (criteriaValues["Urgency"] ?: 0f).toInt(),
+                                risk = (criteriaValues["Risk"] ?: 0f).toInt()
+                            )
+                            taskViewModel.updateTask(updatedTask)
+                        }
+                        navController.popBackStack()
+                    }
+                )
+            }
         }
 
         composable(NavigationDestination.PriorityTaskList.route) {
