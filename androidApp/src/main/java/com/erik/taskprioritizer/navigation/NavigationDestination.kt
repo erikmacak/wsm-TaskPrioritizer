607651362c35c159a2820829e05a3c5af50405b9
Erik Macák <erikmacak026@gmail.com>
2025-05-26 18:44:10 +0200

    Added helper functions for route parameters in NavigationDestination.kt file

--- androidApp/src/main/java/com/erik/taskprioritizer/navigation/NavigationDestination.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/navigation/NavigationDestination.kt
@@ -4,7 +4,13 @@ sealed class NavigationDestination(val route: String) {
     object TaskList : NavigationDestination("task_list")
     object PriorityTaskList: NavigationDestination("priority_task_list")
     object AddTask: NavigationDestination("add_task")
-    object EditTask : NavigationDestination("edit_task")
-    object RemoveTask: NavigationDestination("remove_task")
     object AdjustWeights: NavigationDestination("adjust_weights")
+
+    object EditTask : NavigationDestination("edit_task/{taskId}") {
+        fun withId(taskId: String) = "edit_task/$taskId"
+    }
+
+    object RemoveTask : NavigationDestination("remove_task/{taskId}") {
+        fun withId(taskId: String) = "remove_task/$taskId"
+    }
 }
