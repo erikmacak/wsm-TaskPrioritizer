10add1cddb3ee5523ea489712c1a7e8f9478defa
Erik Macák <erikmacak026@gmail.com>
2025-05-26 21:53:49 +0200

    Removed useless line of code in MainActivity.kt file and MyApplicationTheme.kt file

--- androidApp/src/main/java/com/erik/taskprioritizer/android/MainActivity.kt
+++ androidApp/src/main/java/com/erik/taskprioritizer/android/MainActivity.kt
@@ -3,12 +3,10 @@ package com.erik.taskprioritizer.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
 import com.erik.taskprioritizer.navigation.AppNavigation
 import com.erik.taskprioritizer.ui.theme.TaskPrioritizerTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
