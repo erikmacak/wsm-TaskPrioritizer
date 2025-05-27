750d201f2ad0888b3e025c51a8ec8d94b61a0cee
Erik Macák <erikmacak026@gmail.com>
2025-05-27 16:47:23 +0200

    Fully working task ranking

--- shared/src/commonMain/kotlin/com/erik/taskprioritizer/logic/TaskRanker.kt
+++ shared/src/commonMain/kotlin/com/erik/taskprioritizer/logic/TaskRanker.kt
@@ -3,5 +3,25 @@ package com.erik.taskprioritizer.logic
 import com.erik.taskprioritizer.model.Task
 
 object TaskRanker {
+    fun rankAll(tasks: List<Task>): List<Task> {
+        val sortedTasks = tasks.sortedByDescending { it.getPriorityScore() }
 
+        var currentRank = 1
+        var previousScore: Float? = null
+        var sameRankCount = 0
+
+        return sortedTasks.map { task ->
+            val rank = if (task.getPriorityScore() == previousScore) {
+                currentRank
+            } else {
+                currentRank + sameRankCount
+            }
+
+            sameRankCount = if (task.getPriorityScore() == previousScore) sameRankCount + 1 else 1
+            previousScore = task.getPriorityScore()
+            currentRank = rank
+
+            task.copy(rank = rank)
+        }
+    }
 }
--- shared/src/commonMain/kotlin/com/erik/taskprioritizer/repository/TaskRepository.kt
+++ shared/src/commonMain/kotlin/com/erik/taskprioritizer/repository/TaskRepository.kt
@@ -1,5 +1,6 @@
 package com.erik.taskprioritizer.repository
 
+import com.erik.taskprioritizer.logic.TaskRanker
 import com.erik.taskprioritizer.model.Task
 
 object TaskRepository {
@@ -21,25 +22,7 @@ object TaskRepository {
     }
 
     fun getAllRanked(): List<Task> {
-        val sortedTasks = taskList.sortedByDescending { it.getPriorityScore() }
-
-        var currentRank = 1
-        var previousScore: Float? = null
-        var sameRankCount = 0
-
-        return sortedTasks.mapIndexed { index, task ->
-            val rank = if (task.getPriorityScore() == previousScore) {
-                currentRank
-            } else {
-                currentRank + sameRankCount
-            }
-
-            sameRankCount = if (task.getPriorityScore() == previousScore) sameRankCount + 1 else 1
-            previousScore = task.getPriorityScore()
-            currentRank = rank
-
-            task.copy(rank = rank)
-        }
+        return TaskRanker.rankAll(taskList)
     }
 
     fun getAllByName(name: String): List<Task> {
