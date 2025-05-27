e48c93e080ba61c23eb163eaa11c647bed7e284a
Erik Macák <erikmacak026@gmail.com>
2025-05-27 16:03:30 +0200

    Calculate and recalculateAllTasks functions implemented in TaskScorer.kt file

--- shared/src/commonMain/kotlin/com/erik/taskprioritizer/logic/TaskScorer.kt
+++ shared/src/commonMain/kotlin/com/erik/taskprioritizer/logic/TaskScorer.kt
@@ -3,5 +3,18 @@ package com.erik.taskprioritizer.logic
 import com.erik.taskprioritizer.model.Task
 
 object TaskScorer {
+    fun calculate(task: Task, weights: Map<String, Float>): Float {
+        return (
+            task.getBenefit() * (weights["Benefit"]!!) +
+                    task.getUrgency() * (weights["Urgency"]!!) +
+                    (10 - task.getComplexity()) * (weights["Complexity"]!!) +
+                    (10 - task.getRisk()) * (weights["Risk"]!!)
+        )
+    }
 
+    fun recalculateAllTasks(tasks: List<Task>, weights: Map<String, Float>): List<Task> {
+        return tasks.map { task ->
+            task.copy(priorityScore = calculate(task, weights))
+        }
+    }
 }
