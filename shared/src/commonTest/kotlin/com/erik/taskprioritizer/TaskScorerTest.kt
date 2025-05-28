f2b65f42396b361d32ef9c302637240c9b10453f
Erik Macák <erikmacak026@gmail.com>
2025-05-28 22:43:42 +0200

    Added kotlin-test dependency for commonTest in shared module

---
+++ shared/src/commonTest/kotlin/com/erik/taskprioritizer/TaskScorerTest.kt
@@ -0,0 +1,11 @@
+package com.erik.taskprioritizer
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TaskScorerTest {
+    @Test
+    fun sampleTest() {
+        assertEquals(2, 1 + 1)
+    }
+}
